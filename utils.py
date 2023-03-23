07fffb4146f713bbb80cede6e476f7ee939946f3
Natalia Khaidanova <122217670+NataliaKhaidanova@users.noreply.github.com>
2023-03-23 20:57:03 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,35 @@
+def save_to_json(examples, labels):
+    """
+    Save examples and gold labels in .json.
+    
+    :param list examples: examples 
+    :param list labels: gold labels 
+    :return: None
+    """
+    for i in range(len(examples)):
+        output_dict = {'example': examples[i], 'BIO': labels}
+        with open('Benefactive.json', 'a') as outfile:
+            json.dump(output_dict, outfile)
+            outfile.write('\n')
+            
+def delete_duplicates(json_file):
+    """
+    Read in .json, get none-duplicating examples 
+    
+    :param str json_file: path to .json file
+    :return: a list with none-duplicating examples
+    """
+    all_examples, clean_examples = [], []
+    
+    with open(json_file) as infile:
+        for line in infile.readlines():
+            line = line.strip('\n')
+            example_information = json.loads(line)
+            example = sentence_information['example']
+            all_examples.append(example)
+            
+    for example in all_examples:
+        if example not in clean_examples:
+            clean_examples.append(example)
+            
+    return clean_examples  
